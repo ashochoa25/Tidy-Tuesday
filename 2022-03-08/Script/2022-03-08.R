a9e0671f9d77f987ca681785dc065338f63b8763
Ashlee Ochoa <ashleeochoa@Ashlees-MacBook-Air.local>
2022-03-10 14:32:36 -0800

    commit

--- 2022-03-08/Script/2022-03-08.R
+++ 2022-03-08/Script/2022-03-08.R
@@ -23,33 +23,34 @@ erasmus <- tuesdata$erasmus
 # Or read in the data manually
 
 erasmus <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-03-08/erasmus.csv')
-
+###
 Europe <- rworldmap("Europe")
 spain<- map_data("spain")
 
 ### Plotting 
-EU <- erasmus %>% 
-  filter(academic_year == "2019-2020") %>% 
-  filter(participant_age >= 10,  participant_age <=25) %>% 
-  filter(sending_country_code == "UK") %>% 
+EU <- erasmus %>% ## renaming data set for filtering 
+  filter(academic_year == "2019-2020") %>%  ## filtering specific academic year 
+  filter(participant_age >= 10,  participant_age <=25) %>% ## filter participant age from 10 to 25
+  filter(sending_country_code == "UK") %>% # filter contry code UK 
   filter(sending_organization =="AFRICAN CENTRE FOR DEVELOPMENT AND RESEARCH LTD" |
-         sending_organization =="Boys' and Girls' Clubs of Wales" |
+         sending_organization =="Boys' and Girls' Clubs of Wales" | ## filter for sending organization 
            sending_organization == "WONDER FOUNDATION") %>% 
   select(academic_year, participant_nationality, participant_gender, participant_profile, participant_age, sending_city, receiving_city, sending_country_code, sending_organization) %>% 
-  drop_na()
+  drop_na() ## selecting columns and dropping any NAs
 
  
-EU %>% 
+EU %>% ## plotting data set 
 ggplot() +
-  geom_jitter(aes(x= receiving_city, y= participant_age,
+  geom_jitter(aes(x= receiving_city, y= participant_age, # creating a jitter plot of receiving city by participant age with points filtered by gender 
                  color = participant_gender)) +
-  labs(x= "Recieving Cities", y= "Participant Age", 
+  labs(x= "Recieving Cities", y= "Participant Age", ## creating labels 
        title = "Organizations in the UK",
        color = "Gender") +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90),
-       plot.title = element_text(hjust = 0.25))
+  theme(axis.text.x = element_text(angle = 90), ## adjusting axis labels 
+       plot.title = element_text(hjust = 0.25)) 
 
+ggsave(here("2022-03-08", "Output", "OrganizationsintheUK.png")) ## saving image to output folder 
 
 
 
